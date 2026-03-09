c816d8840fe169e62ed318b269db8bd44d2681cd
agustinlinares <agustinlc88@gmail.com>
2026-03-09 23:45:32 +0100

    Add files via upload

---
+++ Ejercicios-basicos-avanzados/ejercicio-08.js
@@ -0,0 +1,20 @@
+const avengers = [
+  "Hulk",
+  "Thor",
+  "Iron Man",
+  "Captain A.",
+  "Spiderman",
+  "Captain M.",
+];
+
+const findLongestWord = (stringList) => {
+  let longest = "";
+  for (const element of stringList) {
+    if (element.length > longest.length) {
+      longest = element;
+    }
+  }
+  return longest;
+};
+
+console.log(findLongestWord(avengers));
---
+++ Ejercicios-basicos-avanzados/ejercicio-12.js
@@ -0,0 +1,25 @@
+const duplicates = [
+  "sushi",
+  "pizza",
+  "burger",
+  "potatoe",
+  "pasta",
+  "ice-cream",
+  "pizza",
+  "chicken",
+  "onion rings",
+  "pasta",
+  "soda",
+];
+
+const removeDuplicates = (list) => {
+  const uniques = [];
+  for (const item of list) {
+    if (!uniques.includes(item)) {
+      uniques.push(item);
+    }
+  }
+  return uniques;
+};
+
+console.log(removeDuplicates(duplicates));
